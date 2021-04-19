744b03adf4fe297283e3ecfe4da4b9e983748483
Laura Rolon <mrolonmartinez@udesa.edu.ar>
2021-04-19 00:29:00 -0300

    app use changes

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var registroRouter = require('./routes/resgitro');
+var loginRouter = require('./routes/login');
+var productosRouter = require('./routes/productos');
 var app = express();
 
 // view engine setup
@@ -22,6 +24,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/registro', registroRouter);
+app.use('/login', loginRouter);
+app.use('/productos', productosRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
